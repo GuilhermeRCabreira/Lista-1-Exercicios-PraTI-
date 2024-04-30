acc5cbdd22e7b28a51cfe7b0366b71f214f4db17
GuilhermeRCabreira <104570542+GuilhermeRCabreira@users.noreply.github.com>
2024-04-30 01:55:03 -0300

    Upload exercicio_13.js

--- exercicio_13.js
+++ exercicio_13.js
@@ -3,3 +3,44 @@ calcule e mostre a tabuada de 1 até N. Mostre a tabuada na forma:
 1 x N = N
 2 x N = 2N
 3 x N = 3N */
+
+const prompt = require("prompt-sync")();
+
+console.log("------------Tabuada:------------");
+
+let num1, num2, num3, num4, num5;
+
+num1=entrada("Informe o primeiro número: ");
+num2=entrada("Informe o segundo número: ");
+num3=entrada("Informe o terceiro número: ");
+num4=entrada("Informe o quarto número: ");
+num5=entrada("Informe o quinto número: ");
+
+tabuada(num1);
+tabuada(num2);
+tabuada(num3);
+tabuada(num4);
+tabuada(num5);
+
+/////funcoes//////
+function entrada(mensagem) {
+    let numero;
+    do {
+      numero = prompt(mensagem);
+      if (isNaN(numero) ) {
+        console.log("Informe um número válido.");
+      }
+    } while (isNaN(numero) );
+    return numero;
+  }
+
+  function tabuada(numero) {
+    console.log("Para o número: "+numero);
+    for (let i = 0; i <= numero; i++) {
+        let resultado=numero*i;
+
+        console.log(i+" X "+numero+" = "+resultado);
+        
+    }
+    console.log("\n");
+  }
