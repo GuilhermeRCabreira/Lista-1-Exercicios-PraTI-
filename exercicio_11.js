76a7d509186794e9b1e4798b961a4864d079fb64
GuilhermeRCabreira <104570542+GuilhermeRCabreira@users.noreply.github.com>
2024-04-30 01:53:55 -0300

    Upload exercicio_11.js

--- exercicio_11.js
+++ exercicio_11.js
@@ -2,3 +2,31 @@
 cada valor fornecido escrever uma mensagem que indica se cada valor fornecido é PAR
 ou ÍMPAR. O algoritmo será encerrado imediatamente após a leitura de um valor NULO ou
 NEGATIVO. */
+
+const prompt = require("prompt-sync")();
+
+console.log("------------Leitor de pares e ímpares:------------");
+
+let numero;
+
+do {
+    numero = parseInt(prompt("Informe um número inteiro: "));
+    if (!isNaN(numero)&&numero>0) {
+        verificarParOuImpar(numero);
+    } else {if (numero<0) {
+        console.log("Número inválido. Programa encerrado.");
+    } else {
+        console.log("Número inválido. Programa encerrado."); 
+    }
+        
+    }
+} while (numero>0);
+
+////////Função/////////
+function verificarParOuImpar(numero) {
+    if (numero % 2 ===0) {
+        console.log("O número "+numero+" é par");
+    } else {
+        console.log("O número "+numero+" é ímpar");
+    }
+}
