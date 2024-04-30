28d005c0efff011696f762936276eeaf434a92ea
GuilhermeRCabreira <104570542+GuilhermeRCabreira@users.noreply.github.com>
2024-04-30 11:32:32 -0300

    Upload exercicio_16.js

--- exercicio_16.js
+++ exercicio_16.js
@@ -1,2 +1,30 @@
 /* 16. Escreva um algoritmo para imprimir os 50 primeiros número primos maior que 100.
 Obs.: Número primo é aquele divisível somente por 1 e ele mesmo. */
+
+console.log("------------Números primos:------------");
+
+let contador = 0;
+let numero = 101;
+
+while (contador<50) {
+    if (primo(numero)) {
+        console.log(numero);
+        contador++;
+    }
+    numero++;
+}
+
+
+
+function primo(numero) {
+    if (numero<=1) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(numero); i++){
+        if (numero % i === 0) {
+            return false;
+        }
+     
+    }
+    return true;
+}
